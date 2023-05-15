ce0a1398993d76526cb0ece76826a886021e9487
oana maria barsan <oanabarsan@yahoo.com>
2023-05-15 21:34:55 +0300

    create api jwt authorization token scenario

---
+++ tests/jwt-auth-scenario.test.js
@@ -0,0 +1,39 @@
+const { spec, request } = require("pactum");
+const { faker } = require("@faker-js/faker");
+
+const baseUrl = "https://cuddly-buses-roll.loca.lt";
+
+describe("QA-Practice REST - auth flow test suite", () => {
+  let token = "";
+
+  before(async () => {
+    request.setDefaultTimeout(5000);
+
+    const requestBody = {
+        password: "admin",
+        username: "admin",
+      };
+  
+      const resp = await spec()
+        .post(`${baseUrl}/api/v1/simulate/token`)
+        .withHeaders("Content-Type", "application/json")
+        .withBody(requestBody)
+        .expectStatus(200);
+  
+      token = resp.body.token;
+      console.log(token);
+  });
+
+  it("get all employees", async () => {
+    await spec()
+      .get(`${baseUrl}/api/v1/simulate/get/employees`)
+      .withHeaders("Authorization", "Bearer " + token)
+      .expectStatus(200);
+  });
+
+  it("get all employees - unauthorized", async () => {
+    await spec()
+      .get(`${baseUrl}/api/v1/simulate/get/employees`)
+      .expectStatus(401);
+  });
+});
